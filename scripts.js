81d593629a70ef25a07ede1ae981cc536e499a4d
jgalego25 <joao.cruz.galego@tecnico.ulisboa.pt>
2024-08-17 11:12:56 +0100

    added decimal function

--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
 let firstNumber = "";
 let operator = "";
 let secondNumber = "";
+let check = 0;
 
 function add(num1,num2){
-    return num1 + num2;
+    return parseFloat((num1 + num2).toFixed(3));
 }
 
 function subtract(num1, num2){
-    return num1 - num2;
+    return parseFloat((num1 - num2).toFixed(3));
 }
 
 function multiply(num1, num2){
-    return num1 * num2;
+    return parseFloat((num1 * num2).toFixed(3));
 }
 
 function divide(num1, num2){
-    return num1 / num2;
+    return parseFloat((num1 / num2).toFixed(3));
 }
 
 function operate(num1, operator, num2){
@@ -29,29 +30,55 @@ function operate(num1, operator, num2){
 // Access all buttons with the class "number"
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll(".operator");
-const display = document.querySelector(".display");
+const currentOperation = document.querySelector(".currentOperation");
+const previousOperation = document.querySelector(".previousOperation");
 const clearButton = document.querySelector(".clear");
 const deleteButton = document.querySelector(".delete");
 const equalButton = document.querySelector(".equal");
+const decimalButton = document.querySelector(".decimal");
 
 numberButtons.forEach(button => {
-    button.addEventListener("click",() => display.innerText += button.value);
+    button.addEventListener("click",() => {
+        if (check === 0) {
+            currentOperation.innerText = "";
+            check = 1;
+        }
+        if (currentOperation.innerText.length <= 8) currentOperation.innerText += button.value
+    });
 });
 
 operatorButtons.forEach(button => {
     button.addEventListener("click",() => {
-        firstNumber = display.innerText;
-        display.innerText += button.value;
+        if (operator != "") {
+            secondNumber = currentOperation.innerText.split(operator).pop();
+            currentOperation.innerText = operate(parseFloat(firstNumber),operator,parseFloat(secondNumber));
+            previousOperation.innerText = firstNumber + operator + secondNumber;
+        }
+        firstNumber = currentOperation.innerText;
         operator = button.value;
+        check = 0;
     })
 });
 
-clearButton.addEventListener("click", () => display.innerText = "");
+clearButton.addEventListener("click", () => {
+    currentOperation.innerText = "";
+    previousOperation.innerText = "";
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+});
 
-deleteButton.addEventListener("click", () => display.innerText = display.innerText.substring(0, display.innerText.length-1) );
+decimalButton.addEventListener("click", () => {
+    if (!currentOperation.innerText.includes(".")) {
+        currentOperation.innerText += ".";        
+    }
+});
+
+deleteButton.addEventListener("click", () => currentOperation.innerText = currentOperation.innerText.substring(0, currentOperation.innerText.length-1) );
 
 equalButton.addEventListener("click", () => {
-    secondNumber = display.innerText.split(operator).pop()
-    console.log(secondNumber); 
-    display.innerText = operate(parseInt(firstNumber),operator,parseInt(secondNumber));
-})
+    
+    secondNumber = currentOperation.innerText.split(operator).pop(); 
+    currentOperation.innerText = operate(parseFloat(firstNumber),operator,parseFloat(secondNumber));
+    previousOperation.innerText = firstNumber + operator + secondNumber;
+});
